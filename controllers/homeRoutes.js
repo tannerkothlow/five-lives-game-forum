64ca1fcffe4b0919d86b9d653cc2c6d278e8d323
Tanner Kothlow <tannerkothlow@msn.com>
2022-12-06 20:21:04 -0600

    home routes and login routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,20 +10,32 @@ router.get('/', async (req, res) => {
             limit: 5,
             where: {
                 post_type: 'review'
-            }
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
         });
         const guideData = await Post.findAll({
             limit: 5,
             where: {
                 post_type: 'guide'
-            }
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
         });
 
         const reviews = reviewData.map((reviews) => reviews.get({ plain: true }));
         const guides = guideData.map((guides) => guides.get({ plain: true }));
 
         const content = [reviews, guides];
-        
+
         res.render('home', content);
     } catch (err) {
         res.status(400).json(err);
