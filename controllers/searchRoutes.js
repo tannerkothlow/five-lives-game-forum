14c6d8aacf49b1a8029f8e28b23a2527f15b41d7
Tanner Kothlow <tannerkothlow@msn.com>
2022-12-06 21:06:44 -0600

    Basic search routes added

--- controllers/searchRoutes.js
+++ controllers/searchRoutes.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
-const { Post, Genre } = require('../models');
+const { Post, Genre, User } = require('../models');
 
 // GET '/'
 // Finds all genre
 // Render search 
+router.get('/', async (req, res) => {
+    try {
+        const genreData = await Genre.findAll();
+
+        const genres = genreData.map((genres) => genres.get({ plain: true }));
+        res.render('search', genres);
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
 
 // POST '/'
 // Can:
@@ -11,5 +21,58 @@ const { Post, Genre } = require('../models');
 // Look for all submissions with a certain genre
 // Look for all submissions that are reviews, guides, or either
 // Redirects if it finds something, displays a message if nothing
+
+router.post('/', async (req, res) => {
+    try{
+        // If searching by title
+        if (req.body.param == 'title') {
+            const postData = await Post.findAll({
+                where: {
+                    title: req.body.title
+                }
+            });
+
+            if (!postData) {
+                res.status(400).json({ message: 'No submissions found with that title!' });
+                return;
+            }
+
+            const posts = postData.map((posts) => posts.get({ plain: true }));
+
+            // Temp solution until redirects to submissions
+            res.render('submissions', posts);
+            
+        // If searching by genre
+        } else {
+            const genre = await Genre.findOne({
+                where: {
+                    name: req.body.genre
+                },
+                attributes: ['id']
+            });
+
+            // Do something to genre to get the ID int
+            const genreID = genre;
+
+            const postData = await Post.findAll({
+                where: {
+                    genre_id: genreID
+                }
+            });
+
+            if (!postData) {
+                res.status(400).json({ message: 'No submissions found with that title!' });
+                return;
+            }
+
+            const posts = postData.map((posts) => posts.get({ plain: true }));
+
+            // Temp solution until redirects to submissions
+            res.render('submissions', posts);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
