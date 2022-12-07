53fed05f929508f1b54bec3e60abac9b3d66570c
Tanner Kothlow <tannerkothlow@msn.com>
2022-12-06 19:52:20 -0600

    homepage routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,4 +4,30 @@ const { Post, User} = require('../models');
 // GET '/'
 // render home
 
+router.get('/', async (req, res) => {
+    try {
+        const reviewData = await Post.findAll({
+            limit: 5,
+            where: {
+                post_type: 'review'
+            }
+        });
+        const guideData = await Post.findAll({
+            limit: 5,
+            where: {
+                post_type: 'guide'
+            }
+        });
+
+        const reviews = reviewData.map((reviews) => reviews.get({ plain: true }));
+        const guides = guideData.map((guides) => guides.get({ plain: true }));
+
+        const content = [reviews, guides];
+        
+        res.render('home', content);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
 module.exports = router;
