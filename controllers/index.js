bb2bfd6415a5980158d98f2111f5a030cc2621c0
Tanner Kothlow <tannerkothlow@msn.com>
2022-12-08 18:40:25 -0600

    Added logout route API

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,7 @@ const searchRoutes = require('./searchRoutes');
 const registerRoutes = require('./registerRoutes');
 const submissionRoutes = require('./submissionRoutes');
 const newSubmissionRoutes = require('./newSubmissionRoutes');
+const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 router.use('/login', loginRoutes);
@@ -12,6 +13,6 @@ router.use('/search', searchRoutes);
 router.use('/register', registerRoutes);
 router.use('/submissions', submissionRoutes);
 router.use('/new-submission', newSubmissionRoutes);
-
+router.use('/api', apiRoutes);
 
 module.exports = router;
